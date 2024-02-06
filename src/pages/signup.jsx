f06bfa8a1dda3719cea470cdb82b1bbe3749a143
AARON HANNAH <aaronhannah88@yahoo.com>
2024-02-05 20:43:05 -0500

    first iteration of sign up page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { auth } from '../firebase'; // Import Firebase configuration
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
-export default function Signup() {
+const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    if (password.length < 8){
+      setError('Password must be longer than 8 characters')
+    }
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+    }
+  };
+
   return (
-    <h1>Welcome</h1>
-  )
-}
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSignup}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default Signup;
