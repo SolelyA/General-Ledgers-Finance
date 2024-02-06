d86fc20b51122b8a91379560ee599a42af495fd1
AARON HANNAH <aaronhannah88@yahoo.com>
2024-02-06 17:04:51 -0500

    added more input fields for signup

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -6,21 +6,27 @@ const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
 
   const handleSignup = async (e) => {
     e.preventDefault();
+    setError('')
     if (password.length < 8){
       setError('Password must be longer than 8 characters')
     }
-    if (error.code === 'auth/email-already-exists') { 
-      // handle error, if you have multiple possible ones you could probably 
-      // try using switch statement 
-  } 
     try {
       await createUserWithEmailAndPassword(auth, email, password);
-      setError('');
     } catch (error) {
-      setError(error.message)
+      console.log(error.message)
+      if (error.code === 'auth/email-already-in-use') { 
+        setError('Email is already in use')
+        // handle error, if you have multiple possible ones you could probably 
+        // try using switch statement 
+      } else{
+          console.error('Signup Error:', error)
+          setError('An error occured during signup. Please try again later')
+      }
     }
   };
 
@@ -40,6 +46,20 @@ const Signup = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        <input
+          type="first name"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <input
+          type="last name"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+
+
         <button type="submit">Sign Up</button>
       </form>
       {error && <p style={{ color: 'red' }}>{error}</p>}
